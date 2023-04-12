000b3e3af0a2999e05c0bbbe30c6bd52e25b0d9c
akshitdandyan <akshitdandyan5287@gmail.com>
2023-04-12 10:07:17 +0530

    use port from envs

--- lightbond_backend/config/envs.js
+++ lightbond_backend/config/envs.js
@@ -5,3 +5,4 @@ config();
 export const REDIS_PORT = process.env.REDIS_PORT;
 export const REDIS_HOST = process.env.REDIS_HOST;
 export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+export const PORT = process.env.PORT || 5000;
